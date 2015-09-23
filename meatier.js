a73c9bcc4ca6bba0d3c54d77045ed493dee83a47
Matt Bailey <matt.bailey@axonvibe.com>
2015-09-23 22:41:49 +0100

    Add a form to submit new data to the collection.

--- meatier.js
+++ meatier.js
@@ -6,6 +6,18 @@ if (Meteor.isClient) {
       return Recipes.find({});
     }
   });
+
+  Template.body.events({
+    "submit .new-recipe": function(event) {
+      event.preventDefault();
+      var method = event.target.text.value;
+      Recipes.insert({
+        method: method,
+        createdAt: new Date()
+      });
+      event.target.text.value = "";
+    }
+  });
 }
 
 if (Meteor.isServer) {
