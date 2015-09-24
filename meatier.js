9e8b928f24d96cd20474512e1c3047dbd238b4de
Matt Bailey <matt.bailey@axonvibe.com>
2015-09-24 22:22:50 +0100

    Add support for user accounts and add methods for interacting with the Mongo collection.

--- meatier.js
+++ meatier.js
@@ -1,6 +1,8 @@
 Recipes = new Mongo.Collection("recipes");
 
 if (Meteor.isClient) {
+  Meteor.subscribe("recipes");
+
   Template.body.helpers({
     recipes: function() {
       return Recipes.find({});
@@ -11,17 +13,43 @@ if (Meteor.isClient) {
     "submit .new-recipe": function(event) {
       event.preventDefault();
       var method = event.target.text.value;
-      Recipes.insert({
-        method: method,
-        createdAt: new Date()
-      });
+      Meteor.call("addRecipe", method);
       event.target.text.value = "";
     }
   });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+
+
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  Meteor.publish("recipes", function() {
+    return Recipes.find();
   });
 }
+
+
+Meteor.methods({
+  addRecipe: function(method) {
+    if(! Meteor.userId()) {
+      throw new Meteor.Error("not-authorised");
+    }
+
+    Recipes.insert({
+      method: method,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  deleteRecipe: function(id) {
+    if(! Meteor.userId()) {
+      throw new Meteor.Error("not-authorised");
+    }
+
+    Recipes.remove(id);
+  }
+});
